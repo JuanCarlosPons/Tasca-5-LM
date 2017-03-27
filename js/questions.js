073e8e4528c66be997c21607a50dcdfbf461dec8
JuanCarlosPons <juankete3@hotmail.com>
2017-03-27 21:28:43 +0200

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -43,15 +43,6 @@ window.onload = function() {
     xhttp.open("GET", "xml/questions.xml", true);
     xhttp.send();
 	
-	//LEER XSL de xml/questions.xml
-	var xhttp2 = new XMLHttpRequest();
-	xhttp2.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			xslDoc=this.responseXML;
-		}
-	};
-	xhttp2.open("GET", "xml/questions.xsl", true);
-	xhttp2.send();
 	
 }
 
